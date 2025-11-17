a714580a218dccbe55204559ce7b6df408625edf
EGgery <eguzman0086@mymail.lausd.net>
2025-11-17 15:04:28 -0800

    Initial Commit

---
+++ You Are AwesomeTests/You_Are_AwesomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  You_Are_AwesomeTests.swift
+//  You Are AwesomeTests
+//
+//  Created by GUZMAN, ETHAN on 11/4/25.
+//
+
+import Testing
+@testable import You_Are_Awesome
+
+struct You_Are_AwesomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
